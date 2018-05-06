40bd7ae40469e8ebd38d5944bf31599472da8107
Amy <amylrobertson@icloud.com>
2018-05-06 10:54:24 +0100

    Add, subtract, multiply and divide tests passing

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,34 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  });
+
+  it('should add', function () {
+    const result = 4 + 3;
+    assert.strictEqual(result, 7);
+  });
+
+  it('should subtract', function () {
+    const result = 10 - 7;
+    assert.strictEqual(result, 3);
+  });
+
+  it('should multiply', function () {
+    const result = 5 * 3;
+    assert.strictEqual(result, 15);
+  });
+
+  it('should divide', function () {
+    const result = 21 / 3;
+    assert.strictEqual(result, 7);
+  });
+
+});
