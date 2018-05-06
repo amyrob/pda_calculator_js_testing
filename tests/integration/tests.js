804ba662a710d8afb4a6f3b5cf604854d9f02d3f
Amy <amylrobertson@icloud.com>
2018-05-06 12:01:24 +0100

    Tests passing for handling negative, decimal and very large numbers.

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -82,4 +82,39 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('12');
   });
 
+  it('should handle a range of numbers - negative number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2');
+  });
+
+  it('should handle a range of numbers - decimal number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5');
+  });
+
+  it('should handle a range of numbers - very large number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2000000');
+  });
+
+  
+
 });
