a2dc1470c068fbdc162c34e3d87f30d217ff67bb
Amy <amylrobertson@icloud.com>
2018-05-06 11:27:11 +0100

    Unit tests passing

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -31,4 +31,32 @@ describe('calculator', function () {
     assert.strictEqual(result, 7);
   });
 
+  it('should get a number click', function () {
+    calculator.numberClick(2);
+    assert.strictEqual(calculator.runningTotal, 2);
+  });
+
+  it('should get multiple number clicks', function () {
+    calculator.numberClick(2);
+    calculator.numberClick(1);
+    assert.strictEqual(calculator.runningTotal, 21);
+  });
+
+  it('should get operator clicks', function () {
+    calculator.numberClick(1);
+    calculator.operatorClick('+');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    assert.strictEqual(calculator.runningTotal, 3);
+  });
+
+  it('should clear when clearClick used', function () {
+    calculator.numberClick(1);
+    calculator.operatorClick('+');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    calculator.clearClick();
+    assert.strictEqual(calculator.runningTotal, 0);
+  });
+
 });
