932259e6bfe60f61d6338b2a787776ceb9884e1a
Amy <amylrobertson@icloud.com>
2018-05-06 11:48:29 +0100

    Tests passing for multiply and divide

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,7 +16,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2');
   });
 
-  it('should display numbers when numbers clicked', function(){
+  it('should display correct running total', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number3')).click();
     element(by.css('#number2')).click();
@@ -41,4 +41,23 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('7');
   });
+
+  it('should display correct answer when numbers multiplied', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('32');
+  });
+
+  it('should display correct answer when numbers divided', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2');
+  });
+
 });
